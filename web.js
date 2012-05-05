3bcf3862f70e6861170107f5dbc516869e9a2812
rossingram <rosingram@gmail.com>
2012-05-05 15:43:27 -0600

    playing with style

--- web.js
+++ web.js
@@ -5,7 +5,7 @@ var app = express.createServer(express.logger());
 app.get('/', function(request, response) {
   response.render("index.ejs", {layout:false});
 });
-  app.use('/static', express.static(__dirname + '/static')); 
+app.use('/static', express.static(__dirname + '/static')); 
 var port = process.env.PORT || 3000;
 app.listen(port, function() {
   console.log("Listening on " + port);
